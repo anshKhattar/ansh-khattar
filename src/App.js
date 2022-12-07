c9c822cc441a8132a2dcecee58e8c0cc03af6df9
Ansh Khattar <61340441+skylark101@users.noreply.github.com>
2022-12-07 19:00:06 +0530

    code modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="welcome">
+      <h1>Hi there </h1>
+      <p>Welcome to my website</p>
+      </div>
+      <div className="navbar">
+        <ul className="navbar-list">
+          <li><a href="">About</a></li>
+          <li><a href="">Experience</a></li>
+          <li><a href="">Work Samples</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Skills</a></li>
+          <li><a href="">Education</a></li>
+        </ul>
+      </div>
+      <div className="main-area"></div>
+      <div className="social-links"></div>
     </div>
   );
 }
